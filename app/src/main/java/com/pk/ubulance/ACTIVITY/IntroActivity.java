701cc97f630e11b2e58c187f4bcf868f38727e09
Pravin Kandala <pravinkandala@hotmail.com>
2016-10-10 12:42:27 -0400

    Fixed permissions difference for older version and new version.

--- app/src/main/java/com/pk/ubulance/ACTIVITY/IntroActivity.java
+++ app/src/main/java/com/pk/ubulance/ACTIVITY/IntroActivity.java
@@ -2,6 +2,7 @@ package com.pk.ubulance.Activity;
 
 import android.Manifest;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,6 +10,7 @@ import com.pk.ubulance.R;
 
 import agency.tango.materialintroscreen.MaterialIntroActivity;
 import agency.tango.materialintroscreen.MessageButtonBehaviour;
+import agency.tango.materialintroscreen.SlideFragment;
 import agency.tango.materialintroscreen.SlideFragmentBuilder;
 
 public class IntroActivity extends MaterialIntroActivity {
@@ -22,36 +24,38 @@ public class IntroActivity extends MaterialIntroActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addSlide(new SlideFragmentBuilder()
-                        .backgroundColor(R.color.colorPrimaryDark)
-                        .buttonsColor(R.color.background)
-                        .possiblePermissions(new String[]{Manifest.permission.CALL_PHONE, Manifest.permission.ACCESS_COARSE_LOCATION})
-                        .neededPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET})
-                        .image(R.drawable.ic_ubulance_intro)
-                        .title("One touch ride to the hospital")
-                        .description("Disclaimer: If it's real emergency, call 911 ")
-                        .build(),
-                new MessageButtonBehaviour(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Thread loading = new Thread() {
-                            public void run() {
-                                try {
-                                    sleep(SPLASH_DISPLAY_LENGTH);
-                                    Intent main = new Intent(IntroActivity.this, HospitalActivity.class);
-                                    startActivity(main);
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                } finally {
-                                    finish();
-                                }
+        final SlideFragmentBuilder slideFragmentBuilder = new SlideFragmentBuilder()
+                .backgroundColor(R.color.colorPrimaryDark)
+                .buttonsColor(R.color.background);
+        if(Build.VERSION.SDK_INT >= 23)
+            slideFragmentBuilder
+                    .possiblePermissions(new String[]{Manifest.permission.CALL_PHONE, Manifest.permission.ACCESS_COARSE_LOCATION})
+                    .neededPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET});
+        final SlideFragment slider = slideFragmentBuilder
+                .image(R.drawable.ic_ubulance_intro)
+                .title("One touch ride to the hospital")
+                .description("Disclaimer: If it's real emergency, call 911 ")
+                .build();
+        addSlide(slider, new MessageButtonBehaviour(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Thread loading = new Thread() {
+                                    public void run() {
+                                        try {
+                                            sleep(SPLASH_DISPLAY_LENGTH);
+                                            Intent main = new Intent(IntroActivity.this, HospitalActivity.class);
+                                            startActivity(main);
+                                        } catch (Exception e) {
+                                            e.printStackTrace();
+                                        } finally {
+                                            finish();
+                                        }
+                                    }
+                                };
+
+                                loading.start();
                             }
-                        };
-
-                        loading.start();
-                    }
-                }, "Book Uber")
+                        }, "Book Uber")
         );
-
     }
 }
